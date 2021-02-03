d69dd287b93f2a954f30170b79c09032831f6db7
Taoh <tgreen@teselagen.com>
2021-02-03 11:27:26 -0800

    small cleanup

--- src/extension.js
+++ src/extension.js
@@ -12,109 +12,110 @@ const showModulePropNames = require("./showModulePropNames");
 function activate(context) {
   const config = vscode.workspace.getConfiguration("node_require");
 
-  const startPick = function({
+  async function startPick({
     insertAtCursor = false,
     multiple = false,
     destructuring = false,
-    importAll = false
+    importAll = false,
   } = {}) {
-    Promise.join(
+    const [
+      packageDepsArray = [],
+      workspaceDepsArray = [],
+      projectFiles = [],
+    ] = await Promise.all([
       getPackageDeps(),
       getWorkspaceDeps(),
-      getProjectFiles(config)
-    ).then(
-      ([packageDepsArray = [], workspaceDepsArray = [], projectFiles = []]) => {
-        const editor = vscode.window.activeTextEditor;
-        if (!editor) return;
-        const items = [];
-        packageDepsArray.sort().forEach(dep => {
-          items.push({
-            label: dep.label,
-            description: "module",
-            fsPath: null,
-            dirPath: dep.dirPath
-          });
+      getProjectFiles(config),
+    ]);
+
+    const editor = vscode.window.activeTextEditor;
+    if (!editor) return;
+    const items = [];
+
+    packageDepsArray.sort().forEach((dep) => {
+      items.push({
+        label: dep.label,
+        description: "module",
+        fsPath: null,
+        dirPath: dep.dirPath,
+      });
+    });
+
+    workspaceDepsArray.sort().forEach((dep) => {
+      items.push({
+        label: dep.label,
+        description: "workspace module",
+        fsPath: null,
+        dirPath: dep.dirPath,
+      });
+    });
+
+    getCoreModules().forEach((dep) => {
+      items.push({
+        label: dep,
+        description: "core module",
+        fsPath: null,
+      });
+    });
+
+    projectFiles.forEach((dep) => {
+      const rootRelative = dep.fsPath
+        .replace(vscode.workspace.rootPath, "")
+        .replace(/\\/g, "/");
+
+      const label = path.basename(dep.path).match(/^index\./)
+        ? // if it is an index.js file we need to preserve the folder name for context
+          `${path.basename(path.dirname(dep.path))}/${path.basename(dep.path)}`
+        : path.basename(dep.path);
+
+      // don't allow requiring of the file being edited
+      if (editor.document.fileName === dep.fsPath) return;
+      items.push({
+        label,
+        detail: rootRelative,
+        description: "project file",
+        fsPath: dep.fsPath,
+      });
+    });
+
+    if (multiple) {
+      items.unshift({
+        label: "------ Finish Selecting ------",
+        finish: true,
+      });
+    }
+
+    const values = [];
+    const finalizeMultiple = () => {
+      Promise.mapSeries(values, (value) => {
+        return insertRequire(value, insertAtCursor, config);
+      });
+    };
+
+    const showSelectionWindow = (items) => {
+      vscode.window
+        .showQuickPick(items, {
+          placeHolder: "Select dependency",
+          matchOnDescription: true,
+          matchOnDetail: true,
+        })
+        .then((value) => {
+          if (!value) return;
+          if (multiple) {
+            if (value.finish) return finalizeMultiple();
+            values.push(value);
+            items = _.difference(items, values);
+            showSelectionWindow(items);
+          } else if (destructuring) {
+            showModulePropNames(value, insertAtCursor, config);
+          } else {
+            insertRequire(value, insertAtCursor, config, importAll);
+          }
         });
+    };
 
-        workspaceDepsArray.sort().forEach(dep => {
-          items.push({
-            label: dep.label,
-            description: "workspace module",
-            fsPath: null,
-            dirPath: dep.dirPath
-          });
-        });
-
-        getCoreModules().forEach(dep => {
-          items.push({
-            label: dep,
-            description: "core module",
-            fsPath: null
-          });
-        });
-
-        projectFiles.forEach(dep => {
-          const rootRelative = dep.fsPath
-            .replace(vscode.workspace.rootPath, "")
-            .replace(/\\/g, "/");
-
-          const label = path.basename(dep.path).match(/^index\./)
-            ? // if it is an index.js file we need to preserve the folder name for context
-              `${path.basename(path.dirname(dep.path))}/${path.basename(
-                dep.path
-              )}`
-            : path.basename(dep.path);
-
-          // don't allow requiring of the file being edited
-          if (editor.document.fileName === dep.fsPath) return;
-          items.push({
-            label,
-            detail: rootRelative,
-            description: "project file",
-            fsPath: dep.fsPath
-          });
-        });
-
-        if (multiple) {
-          items.unshift({
-            label: "------ Finish Selecting ------",
-            finish: true
-          });
-        }
-
-        const values = [];
-        const finalizeMultiple = () => {
-          Promise.mapSeries(values, value => {
-            return insertRequire(value, insertAtCursor, config);
-          });
-        };
-
-        const showSelectionWindow = items => {
-          vscode.window
-            .showQuickPick(items, {
-              placeHolder: "Select dependency",
-              matchOnDescription: true,
-              matchOnDetail: true
-            })
-            .then(value => {
-              if (!value) return;
-              if (multiple) {
-                if (value.finish) return finalizeMultiple();
-                values.push(value);
-                items = _.difference(items, values);
-                showSelectionWindow(items);
-              } else if (destructuring) {
-                showModulePropNames(value, insertAtCursor, config);
-              } else {
-                insertRequire(value, insertAtCursor, config, importAll);
-              }
-            });
-        };
-
-        showSelectionWindow(items);
-      }
-    );
-  };
+    showSelectionWindow(items);
+  }
 
   context.subscriptions.push(
     vscode.commands.registerCommand("node_require.require", () => {
